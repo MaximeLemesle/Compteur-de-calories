568f8ac74d123197a36830be4376830c85def3ae
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-04-23 10:55:52 +0200

    🔥 Remove useless API fetch | 23/04

--- lib/models/aliment.dart
+++ lib/models/aliment.dart
@@ -1,31 +1,31 @@
 class Aliment {
   final int id;
   final String name;
-  final String image;
 
   factory Aliment.fromGeoJson(Map<String, dynamic> json) {
     final Map<String, dynamic> properties = json['properties'] ?? {};
     final int id = properties['id'];
     final String name = properties['name'];
-    final String image = properties['image'];
 
-    return Aliment(id: id, name: name, image: image,);
+    return Aliment(
+      id: id,
+      name: name,
+    );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
-      'image': image,
     };
   }
 
   factory Aliment.fromJson(Map<String, dynamic> json) {
-    return Aliment(id: json['id'], name: json['name'], image: json['image']);
+    return Aliment(id: json['id'], name: json['name']);
   }
 
   Aliment({
     required this.id,
     required this.name,
-    required this.image,
-  });}
+  });
+}
--- lib/repositories/aliment_repository.dart
+++ lib/repositories/aliment_repository.dart
@@ -3,13 +3,13 @@ import 'package:compteur_cal/models/aliment.dart';
 import 'package:http/http.dart';
 
 class AlimentRepository {
-
   /// Appel des ingredients correspondants à une recherche
   Future<List<Aliment>> fetchAliments(String query) async {
     const String apiKey = 'a798d2cb4ee14472a43a0c8a05f88c26';
     final Response response = await get(Uri.parse(
         'https://api.spoonacular.com/food/ingredients/search?query=$query&apiKey=$apiKey'));
-    if (response.statusCode == 200) {
+    if (response.
+    statusCode == 200) {
       final List<Aliment> aliments = [];
       final Map<String, dynamic> json = jsonDecode(response.body);
       final List<dynamic> results = json['results'];
@@ -18,34 +18,11 @@ class AlimentRepository {
         final Aliment aliment = Aliment(
           id: result['id'],
           name: result['name'],
-          image: result['image'],
         );
         aliments.add(aliment);
       }
 
       return aliments;
-    } else {
-      throw Exception('Error ${response.statusCode}: ${response.body}');
-    }
-  }
-
-  /// Appel des informations pour un ingredient -- A TESTER -- 
-  Future<Aliment> fetchAlimentDetails(int id) async {
-    const String apiKey = 'a798d2cb4ee14472a43a0c8a05f88c26';
-    final Response response = await get(Uri.parse(
-        'https://api.spoonacular.com/food/ingredients/$id/information?apiKey=$apiKey'));
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> json = jsonDecode(response.body);
-      
-      // Parsez les informations de l'ingrédient à partir du JSON ici
-      final Aliment aliment = Aliment(
-        id: json['id'],
-        name: json['name'],
-        image: json['image'],
-        // Ajoutez les autres propriétés de l'ingrédient que vous souhaitez récupérer
-      );
-
-      return aliment;
     } else {
       throw Exception('Error ${response.statusCode}: ${response.body}');
     }
